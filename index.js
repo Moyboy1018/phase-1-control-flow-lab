9dbee81c6d63816a4be3ecaa562aa0869c184300
Moyboy <Vincent.moy94@gmail.com>
2023-11-08 14:29:58 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <= 400){
+    return "This one is on me!";
+  } else if(ride > 400 && ride <= 2000){
+    return "That will be twenty bucks.";
+  } else if(ride > 2000 && ride <= 2500){
+    return "I will gladly take your thirty bucks.";
+  } else if(ride > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let destination = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return destination;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip){
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+      break;
+  }
+  return response;
 }
